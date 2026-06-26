de4583567897b28d6e7fda5a7040355bf696bd67
Deshrajparmar <parmardeshraj44@gmail.com>
2026-06-26 23:37:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout,
+    prompt : "$"
+});
+
+rl.prompt();
+
+rl.on("line",(input) =>{
+    
+})
