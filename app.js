8c40e33360c744b77dd00572e687b8765b22f803
Deshrajparmar <parmardeshraj44@gmail.com>
2026-07-01 00:28:30 +0530

    find executable file

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const readline = require("readline");
+const fs = require("fs");
+const path = require("path");
 
 const rl = readline.createInterface({
     input : process.stdin,
@@ -7,6 +9,27 @@ const rl = readline.createInterface({
 });
 
 rl.prompt();
+
+function findExecutable(cmd){
+
+    const pathEnv = process.env.PATH || "";
+
+    const paths = pathEnv.split(path.delimiter);
+
+    for(const dir of paths){
+
+        const fullPath = path.join(dir, cmd);
+
+        try{
+            fs.accessSync(fullPath, fs.constants.X_OK);
+            return fullPath;
+        }catch(err){}
+    }
+
+    return null;
+}
+
+
 
 rl.on("line",(input) =>{
   input = input.trim();
@@ -23,6 +46,19 @@ rl.on("line",(input) =>{
     rl.prompt();
     return;
  }   
+
+ if(input.startsWith("type ")){
+  const cmd = input.slice(5);
+  const executable = findExecutable(cmd);
+
+  if(executable){
+    console.log(executable);
+  }else{
+    console.log("Not found");
+  }
+  rl.prompt();
+  return;
+ }
 
 
 
